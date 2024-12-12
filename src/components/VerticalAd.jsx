57db5a5a14ac896dc492eb77b6f7607a9c5f05e5
Rishabh Raj <rajrishabh225@gmail.com>
2024-12-12 17:21:02 +0530

    updated link in vertical ad

--- src/components/VerticalAd.jsx
+++ src/components/VerticalAd.jsx
@@ -4,12 +4,12 @@ import { SiGooglenews } from "react-icons/si";
 export default function VerticalAd() {
   return (
     <div className="w-full p-4">
-    <div className="bg-white w-full flex items-center gap-x-2 bg-opacity-25 p-4 md:p-6 rounded-lg shadow-2xl">
+    <a href="https://chat.whatsapp.com/FjdhY98IwT95IVwbEgW1td" target="blank" className="bg-white w-full flex items-center gap-x-2 bg-opacity-25 p-4 md:p-6 rounded-lg shadow-2xl">
       <span className=" text-black text-4xl">
         <SiGooglenews />
       </span>
       Susbscribe us for latest updates.
-    </div>
+    </a>
   </div>
   )
 }
